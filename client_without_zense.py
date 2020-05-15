d2743c3d5ad894f3310e71c3076acef76c1434c9
yuki-inaho <yoshikawa@inaho.co>
2020-05-15 16:21:09 +0900

    Add C++ code(memory leak test)

--- client_without_zense.py
+++ client_without_zense.py
@@ -5,9 +5,9 @@ import rgbd_pb2_grpc
 from utils import bytes_to_ndarray
 import sys
 
-with grpc.insecure_channel('localhost:50051') as channel:
-    stub = rgbd_pb2_grpc.RGBDServiceStub(channel)
-    response = stub.RGBDSend(rgbd_pb2.RGBDRequest())
-    rgb_image = bytes_to_ndarray(response.rgb_image)
-    depth_image = bytes_to_ndarray(response.depth_image)
-    
+while True:
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = rgbd_pb2_grpc.RGBDServiceStub(channel)
+        response = stub.RGBDSend(rgbd_pb2.RGBDRequest())        
+        test = response.rgb_image
+        #depth_image = bytes_to_ndarray(response.depth_image)
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+
+#include "rgbd.pb.h"
+#include "rgbd.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using rgbd::RGBDRequest;
+using rgbd::RGBDReply;
+using rgbd::RGBDService;
+
+// Logic and data behind the server's behavior.
+class RGBDServiceImpl final : public rgbd::RGBDService::Service {
+  Status RGBDSend(ServerContext* context, const RGBDRequest* request,
+                  RGBDReply* reply) override {
+    cv::Mat rgb_img = cv::imread("../image/rgb.png");    
+    int width = rgb_img.cols;
+    int height = rgb_img.rows;
+    int channel = rgb_img.channels();
+    int image_size = rgb_img.total() * rgb_img.elemSize();
+    char *image_char = new char[image_size];
+    std::memcpy(image_char, rgb_img.data, image_size * sizeof(char));
+    delete rgb_img;
+
+    
+    std::string data_rgb_str(reinterpret_cast<char const *>(image_char),
+                            image_size);
+    //reply->mutable_rgb_image().Merge_From;
+    reply->set_rgb_image(data_rgb_str);
+    //reply->set_allocated_rgb_image(&data_rgb_str);
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("localhost:50051");
+  RGBDServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+
+  return 0;
+}
+
